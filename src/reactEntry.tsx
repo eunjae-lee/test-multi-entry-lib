63e9f64dabea6a5d32693dfe292cc34f05519476
Eunjae Lee <hey@eunjae.dev>
2023-06-21 16:32:03 +0200

    add submodules

---
+++ src/reactEntry.tsx
@@ -0,0 +1,3 @@
+export default function TheComponent() {
+  return <p>Hi</p>
+}
---
+++ src/vueEntry.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+let name = "World";
+</script>
+
+<template>
+  <p>Hello {{ name }}</p>
+</template>
